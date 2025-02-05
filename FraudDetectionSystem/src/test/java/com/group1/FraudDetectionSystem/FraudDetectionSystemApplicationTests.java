1f6c3d0aba5eb04b5be506555b1ef50107312cea
Daniel <daniel.ruiz@qaracter.com>
2025-02-05 12:35:58 +0000

    feat :initial commit with project creation

---
+++ FraudDetectionSystem/src/test/java/com/group1/FraudDetectionSystem/FraudDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.FraudDetectionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
