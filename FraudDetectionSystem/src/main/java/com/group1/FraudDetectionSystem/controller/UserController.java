41d6e306ab087b93e8aa444752f94cd22547dc9d
Maist <maistsi@gmail.com>
2025-02-06 10:31:26 +0100

    feat: project structure created

---
+++ FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.group1.FraudDetectionSystem.controller;
+
+import com.group1.FraudDetectionSystem.model.User;
+import com.group1.FraudDetectionSystem.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
