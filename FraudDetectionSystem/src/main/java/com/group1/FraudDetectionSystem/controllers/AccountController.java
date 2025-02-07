e7e9a643c8905d11ad4ea2a72b4590a5f58e3344
Maist <maistsi@gmail.com>
2025-02-07 11:18:57 +0100

    doc: implement java doc AccountController

--- FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/controllers/AccountController.java
+++ FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/controllers/AccountController.java
@@ -7,20 +7,36 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+/**
+ * Controller for handling account-related operations.
+ */
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
     private final AccountService accountService;
 
+    /**
+     * Constructor to initialize AccountService.
+     * @param accountService The service handling account logic.
+     */
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
+    /**
+     * Retrieves all accounts.
+     * @return A collection of all accounts.
+     */
     @GetMapping
     public Collection<Account> getAllUsers() {
         return this.accountService.getAllAccounts();
     }
 
+    /**
+     * Creates a new account.
+     * @param account The account details.
+     * @return A confirmation message with account ID.
+     */
     @PostMapping()
     public String createAccount(@RequestBody Account account) {
         return accountService.createAccount(account);
