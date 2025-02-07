111cdefed9bb5c4bec60ce91c8da9d94872946da
Maist <maistsi@gmail.com>
2025-02-07 11:20:34 +0100

    doc: implement java doc UserController

--- FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/controllers/UserController.java
+++ FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/controllers/UserController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Controller for handling user-related operations.
+ */
 @RestController
 @RequestMapping("/users")
+
+/**
+ * Constructor to initialize UserService.
+ * @param userService The service handling user logic.
+ */
 public class UserController {
     private final UserService userService;
 
@@ -17,12 +25,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * Retrieves all users.
+     * @return A collection of all users.
+     */
     @GetMapping
     public Collection<User> getAllUsers() {
         return this.userService.getAllUsers();
     }
 
-
+    /**
+     * Registers a new user.
+     * @param user The user details.
+     * @return A confirmation message with user ID.
+     */
     @PostMapping("/register")
     public String registerUser(@RequestBody User user) {
         return userService.registerUser(user);
