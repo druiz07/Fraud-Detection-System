0c3d497c027ae21ba6563b24fc414f33457a475b
Maist <maistsi@gmail.com>
2025-02-07 11:26:31 +0100

    doc: implement java doc UserService

--- FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/services/UserService.java
+++ FraudDetectionSystem/src/main/java/com/group1/FraudDetectionSystem/services/UserService.java
@@ -17,9 +17,17 @@ public class UserService {
     private final Map<Long, User> users = new HashMap<>();
     private long userIdCounter = 1;
 
+    /**
+     * Retrieves all users.
+     * @return A collection of all users.
+     */
     public Collection<User> getAllUsers(){return this.users.values();}
 
-
+    /**
+     * Registers a new user.
+     * @param user User details.
+     * @return Confirmation message with user ID.
+     */
     public String registerUser(User user) {
         user.setId(userIdCounter++);
         users.put(user.getId(), user);
